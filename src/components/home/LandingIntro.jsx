3c337e30a55ac2f9a1aadd25b547b8275b302105
Derrick <similienderrick@yahoo.com>
2025-05-02 01:54:47 -0400

    Fix AOS duration for LandingIntro animations

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -6,10 +6,14 @@ const LandingIntro = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
               <i className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
-                <h4 className="">Set up your wallet</h4>
+                <h4>Set up your wallet</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
@@ -19,10 +23,14 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
               <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
-                <h4 className="">Add your NFT's</h4>
+                <h4>Add your NFT's</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
@@ -32,10 +40,14 @@ const LandingIntro = () => {
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
-            <div className="feature-box f-boxed style-3">
+            <div
+              className="feature-box f-boxed style-3"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
               <i className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
-                <h4 className="">Sell your NFT's</h4>
+                <h4>Sell your NFT's</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
